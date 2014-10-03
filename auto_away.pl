0c698b9bd7c86e2c90d47775475f194663a5b5e3
Lukas Mai <l.mai@web.de>
2014-10-03 09:56:53 +0200

    react to timeout changes immediately

--- auto_away.pl
+++ auto_away.pl
@@ -4,7 +4,7 @@ use warnings;
 use Irssi ();
 use Time::HiRes qw(time);
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 our %IRSSI = (
     authors => 'mauke',
     name    => 'auto_away',
@@ -19,23 +19,18 @@ sub on_settings_change (&) {
 Irssi::settings_add_str  $IRSSI{name}, "$IRSSI{name}_message", "zZzZ";
 Irssi::settings_add_time $IRSSI{name}, "$IRSSI{name}_time",    '23m';
 
+my $here_heartbeat;
+my $last_keypress_ts;
+
 my $_message;
 my $_time;
-on_settings_change {
-    $_message = Irssi::settings_get_str  "$IRSSI{name}_message";
-    $_time    = Irssi::settings_get_time "$IRSSI{name}_time";
-};
-
-my $detected_state_away;
-my $last_keypress_ts;
 
 sub start_timer {
     $last_keypress_ts = time;
-    Irssi::timeout_add_once $_time, \&ding, undef;
+    $here_heartbeat = Irssi::timeout_add_once $_time, \&ding, undef;
 }
 
 sub go_away {
-    $detected_state_away = 1;
     for my $srv (Irssi::servers) {
         next if $srv->{chat_type} ne 'IRC' || $srv->{usermode_away};
         $srv->command("away -one $_message");
@@ -43,7 +38,6 @@ sub go_away {
 }
 
 sub come_back {
-    $detected_state_away = 0;
     for my $srv (Irssi::servers) {
         next if $srv->{chat_type} ne 'IRC' || !$srv->{usermode_away};
         $srv->command("away -one");
@@ -54,19 +48,30 @@ sub come_back {
 sub ding {
     my $delta = $_time - int((time - $last_keypress_ts) * 1000);
     if ($delta < 10) {
+        $here_heartbeat = undef;
         go_away;
         return;
     }
 
-    Irssi::timeout_add_once $delta, \&ding, undef;
+    $here_heartbeat = Irssi::timeout_add_once $delta, \&ding, undef;
 }
 
 Irssi::signal_add 'gui key pressed' => sub {
     my ($c) = @_;
-    if ($detected_state_away) {
-        come_back if $c == ord "\n";
-    } else {
+    if ($here_heartbeat) {
         $last_keypress_ts = time;
+    } else {
+        come_back if $c == ord "\n";
+    }
+};
+
+on_settings_change {
+    my $prev_time = $_time;
+    $_message = Irssi::settings_get_str  "$IRSSI{name}_message";
+    $_time    = Irssi::settings_get_time "$IRSSI{name}_time";
+    if ($here_heartbeat && $_time < $prev_time) {
+        Irssi::timeout_remove $here_heartbeat;
+        ding;
     }
 };
 
